7b055a187e75e73b3348f06661c16263cfa06e37
SusmitSurwade <susmit.s.surwade@gmail.com>
2024-02-08 14:28:53 +0530

    azure changes

--- test/ccportal_listing_api.py
+++ test/ccportal_listing_api.py
@@ -8,14 +8,14 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from azure.storage.blob import BlobServiceClient
 
-
 yesterday = (datetime.now().date() - timedelta(days=1)).strftime("%d/%m/%Y")
 # token = "eyJhbGciOiJSUzI1NiIsImtpZCI6IjY5NjI5NzU5NmJiNWQ4N2NjOTc2Y2E2YmY0Mzc3NGE3YWE5OTMxMjkiLCJ0eXAiOiJKV1QifQ.eyJuYW1lIjoiU3VzbWl0IFN1cndhZGUiLCJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vZXMtYWktYXV0aCIsImF1ZCI6ImVzLWFpLWF1dGgiLCJhdXRoX3RpbWUiOjE3MDY4ODI5NTYsInVzZXJfaWQiOiJXQUc4NVhpbXlSY0ZnOFRwa21Hbk9FSWtBTUIzIiwic3ViIjoiV0FHODVYaW15UmNGZzhUcGttR25PRUlrQU1CMyIsImlhdCI6MTcwNzA3NDMyNCwiZXhwIjoxNzA3MDc3OTI0LCJlbWFpbCI6InN1c21pdC5zdXJ3YWRlQGJsZW5oZWltY2hhbGNvdC5jb20iLCJlbWFpbF92ZXJpZmllZCI6ZmFsc2UsImZpcmViYXNlIjp7ImlkZW50aXRpZXMiOnsibWljcm9zb2Z0LmNvbSI6WyI2ZTNiNTcxNy1kZTNlLTRmNGYtYjBlOC02ODc3MzBiNjE3YjUiXSwiZW1haWwiOlsic3VzbWl0LnN1cndhZGVAYmxlbmhlaW1jaGFsY290LmNvbSJdfSwic2lnbl9pbl9wcm92aWRlciI6Im1pY3Jvc29mdC5jb20ifX0.k32njBhaAYv0zM6vSXIcxEVqXsQSja4bbd4PM2I5bChb1kvJjCa9K3qRMamp8KaWEZYcbNOGpSegwt_MCU3dKHOVLokZcafb13WyfbEyy90bQTcCNoI9rf_WmExZfs_foF5Q8NG5dg1reFHoQAWjxyMMzBBfjnBTbvbgj8DqDg-RiOhLZAqoMy3AQuCAdYbELuWl_PP3yeAxxuBEGKn4vDIuO2CwGtIjL9GD0Xbat_K0GsWNhRroN4vLN0RMFp186MRS2SwhM6DvC9btO_NPgb5efAh8pqb0zSdd8vw9tOG0D5b-Xr-YVVG2kEtvt_2TTz2mu1bp-BiDaKkh-OE4mA"
 gurl = f"https://cms.comms-coach.englishscore.com/api/org/2/imported-conversations?filters[startDate]={yesterday}&filters[conversationType][0]=Voice&page=1&sort=-startDate"
 
 final_success_count = 0
 final_transcribed_count = 0
-final_azure_count=0
+final_azure_count = 0
+
 
 # access_token = "eyJhbGciOiJSUzI1NiIsImtpZCI6IjUzZWFiMDBhNzc5MTk3Yzc0MWQ2NjJmY2EzODE1OGJkN2JlNGEyY2MiLCJ0eXAiOiJKV1QifQ.eyJuYW1lIjoiU3VzbWl0IFN1cndhZGUiLCJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vZXMtYWktYXV0aCIsImF1ZCI6ImVzLWFpLWF1dGgiLCJhdXRoX3RpbWUiOjE3MDY4ODI5NTYsInVzZXJfaWQiOiJXQUc4NVhpbXlSY0ZnOFRwa21Hbk9FSWtBTUIzIiwic3ViIjoiV0FHODVYaW15UmNGZzhUcGttR25PRUlrQU1CMyIsImlhdCI6MTcwNzExNTk5MiwiZXhwIjoxNzA3MTE5NTkyLCJlbWFpbCI6InN1c21pdC5zdXJ3YWRlQGJsZW5oZWltY2hhbGNvdC5jb20iLCJlbWFpbF92ZXJpZmllZCI6ZmFsc2UsImZpcmViYXNlIjp7ImlkZW50aXRpZXMiOnsibWljcm9zb2Z0LmNvbSI6WyI2ZTNiNTcxNy1kZTNlLTRmNGYtYjBlOC02ODc3MzBiNjE3YjUiXSwiZW1haWwiOlsic3VzbWl0LnN1cndhZGVAYmxlbmhlaW1jaGFsY290LmNvbSJdfSwic2lnbl9pbl9wcm92aWRlciI6Im1pY3Jvc29mdC5jb20ifX0.N-3d3dz02z9Uvsrl2sPK-gwsA8UL2Q6-iuVNsqSCQ_W3i1YuM7EzOHLNzxrNy64dr1N-EBpyUErRuxzMzX50_GrC96b-dH6AVD6khy56YjU5bIXae0sO-aQOoG3UTFu2EJAEGAIiA9dtDyqpXzzyfZsfNB8JxpYuZ_5NN26PPMgZPqAQCISRnLJ8sWqAhi4DEuHm0qtT37ds-tCvFODlPZr2MGYVPe2xEvP_PbVFiAXrHcGPTdfd2iAWnI6Pyoz7Ica-1Rb7DcuWtQgSHSBE4NawXdCKDJTU1rjqwARJ9feefTWGScF-2bgkCm-OzDbWuMCTW_EjHqQz-__EYCNs1w"
 
@@ -34,26 +34,27 @@ def count_files_with_string(account_name, account_key, container_name, search_st
 
     # Iterate over blobs and count those containing the search string
     for blob in blobs:
-       name=blob.name
-       if search_string in blob.name and name.endswith('.wav'):
-          file_count += 1
+        name = blob.name
+        if search_string in blob.name and name.endswith('.wav'):
+            file_count += 1
     return file_count
 
-    #target_date = datetime.now().replace(tzinfo=None)-timedelta(days=1)
-    #for blob in blobs:
-        #if blob.creation_time and blob.creation_time.replace(tzinfo=None) > target_date:
-            #file_count += 1
-        #return file_count
+    # target_date = datetime.now().replace(tzinfo=None)-timedelta(days=1)
+    # for blob in blobs:
+    # if blob.creation_time and blob.creation_time.replace(tzinfo=None) > target_date:
+    # file_count += 1
+    # return file_count
 
-#if __name__ == "__main__":
-@pytest.mark.skip()
+
+# if __name__ == "__main__":
+# @pytest.mark.skip()
 def test_azure_blob_count():
     global afile_count
     # Replace these with your Azure Storage account details
 
-    #account_name = "susmit"
-    #account_key = "B5gjKGSC0QK8TvPkWvKFSx6+8RbJ6sPwgDQgztIEIodwiekA6yO/eVy+htOBbKGEBsV3SX381Jrh+AStoAR/eA=="
-    #container_name = "susmitcont"
+    # account_name = "susmit"
+    # account_key = "B5gjKGSC0QK8TvPkWvKFSx6+8RbJ6sPwgDQgztIEIodwiekA6yO/eVy+htOBbKGEBsV3SX381Jrh+AStoAR/eA=="
+    # container_name = "susmitcont"
 
     account_name = "escommscoachinbound"
     account_key = "NlaxHb6fioxyYAWOUFhCli4QLRoiPDmTXPrKNuSij+mNQdC+N8WNQxsyRke5xaeM2TGT5R1PWBcz+AStDXnptw=="
@@ -128,11 +129,11 @@ def test_listing_total_count():
     return page_no
 
 
-#pytest.mark.parametrize("page_number", range(1,test_listing_total_count() + 1))
-@pytest.mark.parametrize("page_number", range(1, 3))
+@pytest.mark.parametrize("page_number", range(1, test_listing_total_count() + 1))
+# @pytest.mark.parametrize("page_number", range(1, 3))
 def test_listing_sucess_fail_records(page_number):
-    success_count=0
-    transcribed_count=0
+    success_count = 0
+    transcribed_count = 0
     url = f"https://cms.comms-coach.englishscore.com/api/org/2/imported-conversations?filters[startDate]={yesterday}&filters[conversationType][0]=Voice&page={page_number}&sort=-startDate"
     payload = {}
     headers = {
@@ -169,13 +170,13 @@ def test_listing_sucess_fail_records(page_number):
     global final_transcribed_count
     final_transcribed_count += transcribed_count
     print(f"\n Running test for page {page_number}")
-    #print(final_success_count)
-    #print(final_transcribed_count)
+    # print(final_success_count)
+    # print(final_transcribed_count)
+
 
 def test_print_counts():
     print(f"\n Number of successful conversations ie Status = Report Ready: {final_success_count}")
     print(f"\n Number of transcribed conversations ie Status = In progress: {final_transcribed_count}")
-
 
 
 def test_send_mail():
@@ -186,14 +187,14 @@ def test_send_mail():
     smtp_password = "qzod ltfm nmav tqvw"
 
     # Recipient email address
-    # recipient_email = "susmit.surwade@blenheimchalcot.com"
-    recipient_emails = ["susmit.surwade@blenheimchalcot.com", "susmit.s.surwade@gmail.com"]
+    recipient_emails = ["susmit.surwade@blenheimchalcot.com"]
+    # recipient_emails = ["susmit.surwade@blenheimchalcot.com", "lokesh.singh@blenheimchalcot.com", "ruksar.khan@blenheimchalcot.com"]
 
     # Variables with total count and success count
     total_count = TCount
     success_count = final_success_count
-    transcribed_count=final_transcribed_count
-    #azure_count=afile_count
+    transcribed_count = final_transcribed_count
+    azure_count = afile_count
 
     # Format today's date
     today_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
@@ -207,6 +208,7 @@ def test_send_mail():
         <p><b>Files showing on PRODUCTION CommsCoach portal today as below:</b></p>
         <ul>
           <li>Organisation: Oakbrook</li>
+          <li>Total File Received in Azure storage: {azure_count}</li>
           <li>Total File Received in CMS: {total_count}</li>
           <li>Total Report Ready: {final_success_count}</li>
           <li>Total In progress: {final_transcribed_count}</li>
