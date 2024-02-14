80f5b3b90284bfa6c056977906d119af8290af79
SusmitSurwade <susmit.s.surwade@gmail.com>
2024-02-14 16:23:32 +0530

    all revert h

--- test/ccportal_api_generate_report.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-
-def test_generate_allure_report():
-    allure_results_directory = "C:\\Users\\SusmitSurwade\\PycharmProjects\\commsCoach_API_Automation\\test\\allure_results"
-
-    # Run the allure serve command
-    subprocess.run(["allure", "serve", allure_results_directory])
