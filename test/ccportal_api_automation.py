0b4e09c04f0c9651b13b0254e07b06a51915e28b
SusmitSurwade <susmit.s.surwade@gmail.com>
2024-02-22 16:32:51 +0530

    in progress change

--- test/ccportal_api_automation.py
+++ test/ccportal_api_automation.py
@@ -18,9 +18,9 @@ def delete_files_with_extensions(folder_path, extensions):
 
 # Example usage
 extensions_to_delete = (".json", ".txt")
-#folder_path = r"C:\Users\SusmitSurwade\PycharmProjects\commsCoach_API_Automation\test\allure-results"
+# folder_path = r"C:\Users\SusmitSurwade\PycharmProjects\commsCoach_API_Automation\test\allure-results"
 folder_path = r"./allure-results"
-#@pytest.mark.skip(reason="Skipping this test")
+# @pytest.mark.skip(reason="Skipping this test")
 
 yesterday = (datetime.now().date() - timedelta(days=6)).strftime("%d/%m/%Y")
 # token = "eyJhbGciOiJSUzI1NiIsImtpZCI6IjY5NjI5NzU5NmJiNWQ4N2NjOTc2Y2E2YmY0Mzc3NGE3YWE5OTMxMjkiLCJ0eXAiOiJKV1QifQ.eyJuYW1lIjoiU3VzbWl0IFN1cndhZGUiLCJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vZXMtYWktYXV0aCIsImF1ZCI6ImVzLWFpLWF1dGgiLCJhdXRoX3RpbWUiOjE3MDY4ODI5NTYsInVzZXJfaWQiOiJXQUc4NVhpbXlSY0ZnOFRwa21Hbk9FSWtBTUIzIiwic3ViIjoiV0FHODVYaW15UmNGZzhUcGttR25PRUlrQU1CMyIsImlhdCI6MTcwNzA3NDMyNCwiZXhwIjoxNzA3MDc3OTI0LCJlbWFpbCI6InN1c21pdC5zdXJ3YWRlQGJsZW5oZWltY2hhbGNvdC5jb20iLCJlbWFpbF92ZXJpZmllZCI6ZmFsc2UsImZpcmViYXNlIjp7ImlkZW50aXRpZXMiOnsibWljcm9zb2Z0LmNvbSI6WyI2ZTNiNTcxNy1kZTNlLTRmNGYtYjBlOC02ODc3MzBiNjE3YjUiXSwiZW1haWwiOlsic3VzbWl0LnN1cndhZGVAYmxlbmhlaW1jaGFsY290LmNvbSJdfSwic2lnbl9pbl9wcm92aWRlciI6Im1pY3Jvc29mdC5jb20ifX0.k32njBhaAYv0zM6vSXIcxEVqXsQSja4bbd4PM2I5bChb1kvJjCa9K3qRMamp8KaWEZYcbNOGpSegwt_MCU3dKHOVLokZcafb13WyfbEyy90bQTcCNoI9rf_WmExZfs_foF5Q8NG5dg1reFHoQAWjxyMMzBBfjnBTbvbgj8DqDg-RiOhLZAqoMy3AQuCAdYbELuWl_PP3yeAxxuBEGKn4vDIuO2CwGtIjL9GD0Xbat_K0GsWNhRroN4vLN0RMFp186MRS2SwhM6DvC9btO_NPgb5efAh8pqb0zSdd8vw9tOG0D5b-Xr-YVVG2kEtvt_2TTz2mu1bp-BiDaKkh-OE4mA"
@@ -34,7 +34,7 @@ final_azure_count = 0
 # access_token = "eyJhbGciOiJSUzI1NiIsImtpZCI6IjUzZWFiMDBhNzc5MTk3Yzc0MWQ2NjJmY2EzODE1OGJkN2JlNGEyY2MiLCJ0eXAiOiJKV1QifQ.eyJuYW1lIjoiU3VzbWl0IFN1cndhZGUiLCJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vZXMtYWktYXV0aCIsImF1ZCI6ImVzLWFpLWF1dGgiLCJhdXRoX3RpbWUiOjE3MDY4ODI5NTYsInVzZXJfaWQiOiJXQUc4NVhpbXlSY0ZnOFRwa21Hbk9FSWtBTUIzIiwic3ViIjoiV0FHODVYaW15UmNGZzhUcGttR25PRUlrQU1CMyIsImlhdCI6MTcwNzExNTk5MiwiZXhwIjoxNzA3MTE5NTkyLCJlbWFpbCI6InN1c21pdC5zdXJ3YWRlQGJsZW5oZWltY2hhbGNvdC5jb20iLCJlbWFpbF92ZXJpZmllZCI6ZmFsc2UsImZpcmViYXNlIjp7ImlkZW50aXRpZXMiOnsibWljcm9zb2Z0LmNvbSI6WyI2ZTNiNTcxNy1kZTNlLTRmNGYtYjBlOC02ODc3MzBiNjE3YjUiXSwiZW1haWwiOlsic3VzbWl0LnN1cndhZGVAYmxlbmhlaW1jaGFsY290LmNvbSJdfSwic2lnbl9pbl9wcm92aWRlciI6Im1pY3Jvc29mdC5jb20ifX0.N-3d3dz02z9Uvsrl2sPK-gwsA8UL2Q6-iuVNsqSCQ_W3i1YuM7EzOHLNzxrNy64dr1N-EBpyUErRuxzMzX50_GrC96b-dH6AVD6khy56YjU5bIXae0sO-aQOoG3UTFu2EJAEGAIiA9dtDyqpXzzyfZsfNB8JxpYuZ_5NN26PPMgZPqAQCISRnLJ8sWqAhi4DEuHm0qtT37ds-tCvFODlPZr2MGYVPe2xEvP_PbVFiAXrHcGPTdfd2iAWnI6Pyoz7Ica-1Rb7DcuWtQgSHSBE4NawXdCKDJTU1rjqwARJ9feefTWGScF-2bgkCm-OzDbWuMCTW_EjHqQz-__EYCNs1w"
 
 def test_refresh_token_api():
-    #delete_files_with_extensions(folder_path, extensions_to_delete)
+    # delete_files_with_extensions(folder_path, extensions_to_delete)
     global access_token
     url = "https://securetoken.googleapis.com/v1/token?key=AIzaSyCz1XmsF38COosTR5crCZh8p8ZsAVCaNgM"
 
@@ -137,10 +137,9 @@ def test_listing_api_conversation_status_success_transcribed_page_no(page_number
     final_success_count += success_count
     global final_transcribed_count
     final_transcribed_count += transcribed_count
-    #print(f"\n Running test for page {page_number}")
+    # print(f"\n Running test for page {page_number}")
     print(f"\n success records on page 1 = {final_success_count}")
     print(f"\n transcribed records on page 1 = {final_transcribed_count}")
-
 
 
 def test_listing_api_Total_files_Count_less_than_10min():
@@ -168,6 +167,7 @@ def test_listing_api_Total_files_Count_less_than_10min():
     DCount = json_response["meta"]["total"]
     print("\n Total Count of file with duration less than 10 mins are: " + str(DCount))
 
+
 def test_listing_api_Total_files_Count_greater_than_10min():
     url = f"https://cms.comms-coach.englishscore.com/api/org/2/imported-conversations?filters[startDate]={yesterday}&filters[endDate]={yesterday}&filters[conversationType][0]=Voice&filters[duration][min]=00:10:00&filters[duration][max]=00:30:00&page=1&sort=-startDate"
     payload = {}
@@ -192,6 +192,7 @@ def test_listing_api_Total_files_Count_greater_than_10min():
     assert response.status_code == 200
     DCount = json_response["meta"]["total"]
     print("\n Total Count of file with duration greater than 10 mins are: " + str(DCount))
+
 
 def test_listing_api_Total_no_of_files_this_week():
     url = f"https://cms.comms-coach.englishscore.com/api/org/2/imported-conversations?filters[startDate]={(datetime.today() - timedelta(days=(datetime.today().weekday() - 0) % 7)).strftime('%d/%m/%Y')}&filters[conversationType][0]=Voice&page=1&sort=-startDate"
@@ -218,6 +219,7 @@ def test_listing_api_Total_no_of_files_this_week():
     WCount = json_response["meta"]["total"]
     print("\n This Week Total Count of file is " + str(WCount))
 
+
 def test_listing_api_Total_no_of_files_this_month():
     url = f"https://cms.comms-coach.englishscore.com/api/org/2/imported-conversations?filters[startDate]={(datetime.today().replace(day=1)).strftime('%d/%m/%Y')}&filters[conversationType][0]=Voice&page=1&sort=-startDate"
     payload = {}
@@ -243,6 +245,7 @@ def test_listing_api_Total_no_of_files_this_month():
     MCount = json_response["meta"]["total"]
     print("\n This Month Total Count of file is " + str(MCount))
 
+
 def test_conversation_detail_api_verify_Report_ready_status():
     import requests
     url = "https://cms.comms-coach.englishscore.com/api/org/2/imported-conversations/13238"
@@ -267,8 +270,33 @@ def test_conversation_detail_api_verify_Report_ready_status():
     assert response.status_code == 200
     json_response = json.loads(response.text)
     reportready = json_response["status"]
-    print("status is "+reportready)
-    assert reportready=="report_ready"
+    print("status is " + reportready)
+    assert reportready == "report_ready"
 
 
+def test_conversation_detail_api_verify_In_Progress_status():
+    import requests
+    url = "https://cms.comms-coach.englishscore.com/api/org/2/imported-conversations/13191"
+    payload = {}
+    headers = {
+        'Accept': 'application/json, text/plain, */*',
+        'Accept-Language': 'en-US,en;q=0.9',
+        'Authorization': f'Bearer {access_token}',
+        'Connection': 'keep-alive',
+        'Origin': 'https://comms-coach.englishscore.com',
+        'Referer': 'https://comms-coach.englishscore.com/',
+        'Sec-Fetch-Dest': 'empty',
+        'Sec-Fetch-Mode': 'cors',
+        'Sec-Fetch-Site': 'same-site',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36',
+        'sec-ch-ua': '"Not A(Brand";v="99", "Google Chrome";v="121", "Chromium";v="121"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"'
+    }
 
+    response = requests.request("GET", url, headers=headers, data=payload)
+    assert response.status_code == 200
+    json_response = json.loads(response.text)
+    In_progress = json_response["status"]
+    print("status is " + In_progress)
+    assert In_progress == "in_progress"
