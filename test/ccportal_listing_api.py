c899c54e49e3963bb865e0281934b40a72e45199
SusmitSurwade <susmit.s.surwade@gmail.com>
2024-03-05 19:31:21 +0530

    other mail change 2

--- test/ccportal_listing_api.py
+++ test/ccportal_listing_api.py
@@ -190,8 +190,8 @@ def test_send_mail():
     smtp_password = "qzod ltfm nmav tqvw"
 
     # Recipient email address
-    recipient_emails = ["susmit.surwade@blenheimchalcot.com"]
-    #recipient_emails = ["rinkesh.das@englishscore.com","ami.jambusaria@englishscore.com","susmit.surwade@blenheimchalcot.com", "lokesh.singh@blenheimchalcot.com", "ruksar.khan@blenheimchalcot.com","ami.jambusaria@blenheimchalcot.com","rinkesh.das@blenheimchalcot.com"]
+    #recipient_emails = ["susmit.surwade@blenheimchalcot.com"]
+    recipient_emails = ["rinkesh.das@englishscore.com","ami.jambusaria@englishscore.com","susmit.surwade@blenheimchalcot.com", "lokesh.singh@blenheimchalcot.com", "ruksar.khan@blenheimchalcot.com","ami.jambusaria@blenheimchalcot.com","rinkesh.das@blenheimchalcot.com"]
 
     # Variables with total count and success count
     total_count = TCount
