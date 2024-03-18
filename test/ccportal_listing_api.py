0ce775f6033ea50d87555b38f6d3bdd50fcc4609
SusmitSurwade <susmit.s.surwade@gmail.com>
2024-03-18 15:01:57 +0530

    email updated

--- test/ccportal_listing_api.py
+++ test/ccportal_listing_api.py
@@ -192,7 +192,8 @@ def test_send_mail():
     sender_email = 'no-reply@mail.englishscore.com'
     # Recipient email address
     #recipient_emails = ["susmit.surwade@blenheimchalcot.com"]
-    recipient_emails = ["satyendra.kumar@blenheimchalcot.com","jeff.miranda@blenheimchalcot.com","susmit.surwade@blenheimchalcot.com", "lokesh.singh@blenheimchalcot.com", "ruksar.khan@blenheimchalcot.com","ami.jambusaria@blenheimchalcot.com","rinkesh.das@blenheimchalcot.com"]
+    recipient_emails = ["satyendra.kumar@blenheimchalcot.com","jeff.miranda@blenheimchalcot.com","susmit.surwade@blenheimchalcot.com", "lokesh.singh@blenheimchalcot.com","ami.jambusaria@blenheimchalcot.com","rinkesh.das@blenheimchalcot.com",
+                        "help@maxcontact.com","automation@maxcontact.com","dialler.team@sigmaconnected.com","vincent.khomola@sigmaconnected.com","jenna.barnes@sigmaconnected.com","nathier.davids@sigmaconnected.com"]
 
     # Variables with total count and success count
     total_count = TCount
